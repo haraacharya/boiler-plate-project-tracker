c90ada7dd27077f9b37b12fed2b1785a50a54f6c
hara <hara.acharya@gmail.com>
2016-08-19 09:54:41 -0700

    initial commit

--- both/accounts/config.js
+++ both/accounts/config.js
@@ -1,3 +1,17 @@
 AccountsTemplates.configureRoute('signIn', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('signUp', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('ensureSignedIn', {layoutTemplate: 'appLayout'});
+
+
+AccountsTemplates.addFields([
+    {
+        _id: 'ustid',
+        type: 'text',
+        displayName: "UST_ID",
+    },
+    {
+        _id: 'intelid',
+        type: 'text',
+        displayName: "INTEL_ID",
+    }
+]);
